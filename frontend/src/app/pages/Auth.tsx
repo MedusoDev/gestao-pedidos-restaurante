2625e018e1da53251d2198fbde20837156e05400
Anderson Djalma <122568258+andersondjalma@users.noreply.github.com>
2026-05-22 23:53:39 -0300

    design login

--- frontend/src/app/pages/Auth.tsx
+++ frontend/src/app/pages/Auth.tsx
@@ -1,26 +1,35 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/app/components/ui/card'
 import { LoginForm } from '@/app/components/LoginForm'
 
 export function Auth() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Card className="w-[400px]">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>
-            Acesse sua conta para gerenciar o estabelecimento.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
+    <div className="flex min-h-screen">
+      {/* Lado esquerdo — formulário */}
+      <div className="flex w-full flex-col items-center justify-center px-8 md:w-1/2">
+        <div className="w-full max-w-sm space-y-8">
+          {/* Logo */}
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex items-center gap-2">
+               <img
+                src="https://6a11010be92048e6f022f1f5.imgix.net/pedido-certo.png"
+                alt="logo-pedido-certo"
+                className="h-25 w-auto object-cover"
+              />
+            </div>
+            <h1 className="mt-6 text-2xl font-bold text-gray-500">Boas-vindas</h1>
+          </div>
+
           <LoginForm />
-        </CardContent>
-      </Card>
+        </div>
+      </div>
+
+      {/* Lado direito — imagem */}
+      <div className="hidden md:block md:w-1/2">
+        <img
+          src="https://6a11010be92048e6f022f1f5.imgix.net/pedido-certo-imagem-inicial.png"
+          alt="Restaurante"
+          className="h-full w-full object-cover"
+        />
+      </div>
     </div>
   )
 }
